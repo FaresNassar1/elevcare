f27dfd9de9330c9f6b452e3c5cfa30b7eec696a9
FaresNassar <aborafat_999@hotmail.com>
2024-03-18 10:42:22 +0200

    update

--- modules/Frontend/resources/views/templates/homepage/services.blade.php
+++ modules/Frontend/resources/views/templates/homepage/services.blade.php
@@ -1,35 +1,12 @@
 <style nonce="{{ csp_nonce() }}">
     .bg-services {
-        position: relative;
         background-image: linear-gradient(rgba(0, 0, 0, 0.33), rgba(0, 0, 0, 0.353)), url('{{ asset('front/assets/images/blure.jpg') }}');
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-        min-height: 40vh;
-        padding: 20px; /* Add padding to the container for spacing */
+
     }
 
 
 
 
-    .services-list {
-        list-style-type: none; /* Remove default list styles */
-        padding: 0;
-    }
-
-    .services-list li {
-        margin-bottom: 20px;
-        font-size: 20px
-    }
-
-
-    .image1 {
-        position: absolute;
-    left: 5%;
-    top: -20px;
-    height: 500px;
-    filter: drop-shadow(0 10px 10px rgba(0, 0, 0, 0.309));
-    }
 
 </style>
 
